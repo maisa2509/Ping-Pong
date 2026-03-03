412272d8259ea252b94f8a262bcd4d596dc4e421
maisa2509 <maisa.silverio@escola.pr.gov.br>
2026-03-03 11:28:44 -0300

    update

--- pingpong.js
+++ pingpong.js
@@ -39,33 +39,50 @@ function setup (){
 function draw(){
   backgraund(0);
   movimentoBolinha(0);
-  raquete(xRaquete,yRaquete)
+  raquete(xRaquete,yRaquete);
   raquete(xRaqueteOponenete,
           yRaqueteOponente);
   npc(dificuldade);
-}
-  if(KeyIsDown(UP_ARROW)){}
-   yRaquete -=10
+
+  if(KeyIsDown(UP_ARROW)){
+   yRaquete -=10}
 
   if(KeyIsDown(DOWN_ARROW)){
     yRaquete+= 10}
-
+  
   if(xBolinha -raio< xRaquete + 10 &
-     yBolinha-raio< yRaquete + 90 &)
-     yBolinha + raio> yRaquete )}
+     yBolinha-raio< yRaquete + 90 &
+     yBolinha + raio> yRaquete ){
      velocidadeXBolinha *= -1;
      raquete.play()}
-     }
   if(XBolinha + raio > xRaqueteOponenete &
      yBolinha - raio < yRaqueteOponente + 90 &
-     yBolinha + raio > yRaqueteOponente)
+     yBolinha + raio > yRaqueteOponente){
     velocidadeXBolinha *= -1;
 raquetada.play()}
-     
-     
-     
-     
-     
+  
+  textSize(18)
+  textAlign(CENTER);
+  fill(color(255,0,233));
+  rect(150,10,40,20);
+  fill(255);
+  text(meusPontos,170,26);
+  fill(color(255,0,233));
+  rect(450,10,40,20);
+  fill(255);
+  text(pontosOponenete,470,26);
+  
+  if(xBolinha>595){
+    meusPontos += 1;
+    pontos.play()
+}
+  if(xBolinha<10){
+    pontosOponente +=1;
+    ponto.play()
+  }
+  
+}
+        
 function movimentoBolinha(){
   circle(xBolinha,yBolinha,diamantro);
     
@@ -73,7 +90,26 @@ function movimentoBolinha(){
     yBolinha += velocidadeYBolinha;
     if(xBolinha + raio > width || xBolinha-raio < 0){
       velocidadeXBolinha *= -1;
+      
     }
     if (yBolinha + raio > height || yBolinha-raio< 0){
-      velocidadeYBolinha *= -1}}
-  
+      velocidadeYBolinha *= -1}
+}
+
+function npc(xRaquete,yRaquete){
+  rect(xRaquete,yRaquete,10,90);
+   
+}
+
+function npc(){
+  var nivel = 30;
+  if(deficuldade === "Facil" ){
+    nivel = 100;
+  }
+  if(dificuldade === "Normal"){
+  nivel= 30
+  }
+  let velocidadeYRaqueteOponenete = yBolinha -
+      yRaqueteOponenete - 10 / 2;
+yRaqueteOponente += velocidadeYRaqueteOponenete - nivel;
+  }
